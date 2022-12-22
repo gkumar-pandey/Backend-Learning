bd5c43334518632307b828cf8c776d29e4f0feb8
gkumar=pandey <gautamshekhar078@gmail.com>
2022-12-23 00:45:24 +0530

    learn : read and write file in async way

---
+++ Day-1/Notes.md
@@ -0,0 +1,1 @@
+## Fs Moudule in NodeJs
--- Day-1/index.js
+++ Day-1/index.js
@@ -1,14 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 //* fs module for handling file in node js
-const fileText = fs.readFileSync('./files/file.txt', 'utf-8');
-//? readFileSync is function used to read the file in synchronous way 
+const fileText = fs.readFileSync("./files/file.txt", "utf-8");
+//? readFileSync is function used to read the file in synchronous way
 
-console.log(fileText)
+// console.log(fileText);
 
-//! Now write into file 
+//! Now write into file
 
-const textout = `${fileText} i am a engg student`
+const textout = `${fileText} i am a engg student`;
 
-fs.writeFileSync('./files/outputfile.txt', textout);
+fs.writeFileSync("./files/outputfile.txt", textout);
 
-console.log("file is written")
+// console.log("file is written");
+
+// ** File read and write in asynchronus way
+//? Read file
+fs.readFile("./files/file.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("file is written");
+
+//? Write File
+const writeInFile = "How are your? ";
+fs.writeFile(
+  "./files/outputfile.txt",
+  `${writeInFile}`,
+  "utf-8",
+  (err, data) => {
+    console.log("file is written now");
+  }
+);
